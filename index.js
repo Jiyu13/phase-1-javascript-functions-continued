c733ffd1e411f881623c73e8860f0dbabb044659
Ziru Zou <jiyucoding@yahoo.com>
2023-01-09 14:58:43 -0500

    create mondayFun()

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 function saturdayFun(thingsToDo = "roller-skate") {
     return `This Saturday, I want to ${thingsToDo}!`;
 }
+
+
+const mondayWork = function(thingsToDo = "go to the office") {
+    return `This Monday, I will ${thingsToDo}.`;
+}
